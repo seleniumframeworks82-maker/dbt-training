e57471d265c8f4b63b9498e93cfb186715b687c4
seleniumframeworks82@gmail.com <seleniumframeworks82@gmail.com>
2026-05-25 21:16:27 +0000

    added delivery team seed

--- models/raw/test.sql
+++
@@ -1,3 +0,0 @@
-{% for i in range(10) %}
-    select  {{ i }}  as number {% if not lopp.last %} union all {% endif %}
-{% endfor %}    
--- models/staging/stg_order.sql
+++ models/staging/stg_order.sql
@@ -25,6 +25,6 @@ on o.customerid= c.customerid
 left join {{ ref('raw_product') }} as p
 on o.productid =p.productid
 left join {{ref ('delivery_team')}} as d
-on o.shipmode = d.shipmode
+on o.shipmode
 
 
