479a0b7169040d3feaa319d5be16a32d423df570
seleniumframeworks82@gmail.com <seleniumframeworks82@gmail.com>
2026-05-25 19:14:57 +0000

    added unique and not null test src_globalmart

--- models/raw/test.sql
+++ models/raw/test.sql
@@ -2,7 +2,7 @@ select
     orderid as unique_field,
     count(*) as n_records
 
-from RAW.globamart.raw_order
+from raw.globamart.orders
 where orderid is not null
 group by orderid
 having count(*) > 1
