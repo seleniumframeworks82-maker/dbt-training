947466f54dd39a514e19af07ca18e3f8068cbc64
seleniumframeworks82@gmail.com <seleniumframeworks82@gmail.com>
2026-05-27 10:18:44 +0000

    updated stg_order

--- models/staging/stg_order.sql
+++ models/staging/stg_order.sql
@@ -25,4 +25,4 @@ on o.customerid= c.customerid
 left join {{ ref('raw_product') }} as p
 on o.productid =p.productid
 left join {{ref ('delivery_team')}} as d
-on o.shipmode
+on o.shipmode = d.shipmode
